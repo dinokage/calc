4aaf69f7c0d35db2d1258ffef3c2a452e97bdb79
dinokage <yerradinesh01@gmail.com>
2023-10-20 19:28:45 +0530

    added basic logic and UI elements

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import logo from './logo.svg';
+import {useState} from 'react'
 import './App.css';
 
 function App() {
+  const [input, setInput] = useState(0)
+  const[result, setResult] = useState(0) 
+    const handleChange = (e) => {
+    e.preventDefault();
+    setInput(e.target.value)
+  }
+
+  const Calc = () => {
+    return (
+      <div>
+        <h1>{result}</h1>
+        <input type='number' onChange={handleChange} value={input} />
+      </div>
+    )
+  }
+
+  const add = () => {
+    setResult(result+input)
+  }
+  const sub = () => {
+    setResult(result-input)
+  }
+  const mul = () => {
+    setResult(result*input)
+  }
+  const div = () => {
+    setResult(result/input)
+  }
+
+  const Button = ({type, action}) => {
+    return <button onClick={action}>{type}</button>
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Calc />
+      <Button type="addition" action={add} />
+      <Button type="subtraction" action={sub} />
+      <Button type="multiplication" action={mul}/>
+      <Button type="division" action={div}/>
     </div>
   );
 }
-
 export default App;
